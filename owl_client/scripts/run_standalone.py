7d7bd9b77291fd913c29fae14e77217185b66ffc
dewert <Ewert_99>
2020-07-02 16:24:56 -0700

    Added an additional configuration option to allow for owl pipeline to be able to be run on different types of clusters

--- owl_client/scripts/run_standalone.py
+++ owl_client/scripts/run_standalone.py
@@ -8,9 +8,13 @@ import time
 from argparse import Namespace
 from contextlib import closing
 import traceback
+import voluptuous as vo
+from typing import Callable, Dict, Any, List, Union
 
-from distributed import Client, LocalCluster
+from distributed import Client, LocalCluster, SpecCluster
+from dask_jobqueue import *
 
+from owl_client.formatting import ArithmeticFormatter
 from owl_client.utils import find_free_port, get_pipeline, read_config
 
 logconf = """
@@ -59,12 +63,85 @@ def terminate(*args):  # pragma: nocover
     sys.exit(1)
 
 
+clusterList = [MoabCluster, PBSCluster, SLURMCluster, SGECluster, LSFCluster, OARCluster, HTCondorCluster]
+clusterMap: Dict[str, Callable[..., SpecCluster]] = {c.config_name: c for c in clusterList}
+
+clusterSchema = vo.Schema({
+    vo.Required("type"): str,
+    vo.Required("cores", default="{threads}"): str,
+    vo.Required("memory", default="{memory}GB"): str,
+    vo.Required("local_directory", default="{local_directory}"): str,
+    vo.Required("dashboard_address", default="localhost:{port + 1}"): str,
+    vo.Required("walltime", default="1500000"): str,
+    vo.Required("n_workers", default="{workers}"): str
+}, extra=vo.ALLOW_EXTRA)
+
+formatter = ArithmeticFormatter()
+
+
+def format_conf(obj: Union[Dict, List, str], **kwargs):
+    if isinstance(obj, List):
+        return [format_conf(e, **kwargs) for e in obj]
+    elif isinstance(obj, Dict):
+        return {k: format_conf(v, **kwargs) for k, v in obj.items()}
+    elif isinstance(obj, str):
+        f = formatter.format(obj, **kwargs)
+        try:
+            return int(f)
+        except ValueError:
+            return f
+    else:
+        return obj
+
+
+def createCluster(conf: Dict[str, Any], port, local_directory) -> SpecCluster:
+    f"""
+    Creates a cluster based on "cluster" section of the configuration file
+    uses it's "type" attribute to determine the type of cluster to use
+    and the rest if it's attributes a kwargs to the chosen constructor see below.
+    These attributes will be formatted with the :func:'~formatting.ArithmeticFormatter
+    using the kwargs: [port, local_directory] referring the the parameters of the function
+    and [threads, memory, workers] referring to attributes in the recourse section of the config file.
+    
+    The available types are:
+    {clusterMap}
+
+    If the "cluster" section doesn't exit a local cluster will be created
+    :param conf: configuration file
+    :param port: default port
+    :param local_directory: temporary local directory
+    :return: the cluster
+    """
+    resources = conf.get('resources', {})
+    n_workers = resources.get('workers', 2)
+    threads = resources.get('threads', 2)
+    memory = resources.get('memory', 7)
+    if "cluster" in conf:
+        cluster_conf: Dict[str, Any] = clusterSchema(conf["cluster"])
+        cluster_conf = format_conf(cluster_conf, threads=threads, memory=memory, port=port,
+                                   local_directory=local_directory, workers=n_workers)
+        cluster_type: str = cluster_conf.pop("type")
+        if cluster_type not in clusterMap:
+            raise ValueError(f"{cluster_type} is not a valid type of cluster \n Please use on of {clusterMap.keys()}")
+        constructor = clusterMap[cluster_type]
+        return constructor(**cluster_conf)
+    else:
+        return LocalCluster(
+            n_workers=n_workers,
+            threads_per_worker=threads,
+            scheduler_port=port,
+            dashboard_address='localhost:{}'.format(port + 1),
+            memory_limit='{}GB'.format(memory),
+            local_directory=local_directory
+        )
+
+
 def run_standalone(args: Namespace) -> None:  # pragma: nocover
     """Run worker pipeline.
 
     Parameters
     ----------
-    arg
+    args
         Argparse namespace containing command line flags.
     """
 
@@ -82,22 +159,10 @@ def run_standalone(args: Namespace) -> None:  # pragma: nocover
 
     port = find_free_port()
 
-    resources = conf['resources']
-    n_workers = resources.get('workers', 2)
-    threads = resources.get('threads', 2)
-    memory = resources.get('memory', 7)
-
     tmpdir = os.environ.get('TMP_DIR', '/tmp')
     with tempfile.TemporaryDirectory(dir=tmpdir) as local_directory:
         with closing(
-            LocalCluster(
-                n_workers=n_workers,
-                threads_per_worker=threads,
-                scheduler_port=port,
-                dashboard_address='localhost:{}'.format(port + 1),
-                memory_limit='{}GB'.format(memory),
-                local_directory=local_directory
-            )
+                createCluster(conf, port, local_directory)
         ) as cluster:
             log.info('Running diagnostics interface in http://localhost:%s', port + 1)
             log.info('Starting pipeline %r', conf['name'])
